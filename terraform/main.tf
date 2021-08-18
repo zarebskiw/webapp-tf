c8cc0d7e1a0ccf476e1929efaade4a3e3bea40e1
Florian Lierenfeld <florian.lierenfeld@capgemini.com>
2021-08-18 23:35:54 +0200

    switched to windows node 10 lts

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_app_service_plan" "sp1" {
   name                = "spflolie4123"
   location            = data.azurerm_resource_group.wsdevops.location
   resource_group_name = data.azurerm_resource_group.wsdevops.name
-  
+
   sku {
     tier = "Standard"
     size = "S1"
@@ -42,7 +42,7 @@ resource "azurerm_app_service" "website" {
 
   site_config {
     windows_fx_version = "NODE|10-lts"
-    scm_type         = "LocalGit"
+    scm_type           = "LocalGit"
   }
 }
 

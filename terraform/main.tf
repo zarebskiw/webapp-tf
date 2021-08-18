e98e87ad348b995dee3af279e4efb841e36ad5dd
Florian Lierenfeld <florian.lierenfeld@capgemini.com>
2021-08-18 16:27:31 +0200

    added Draft for Main Terraform Script

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "ws-devops"
+    storage_account_name = "cgmsgtf"
+    container_name       = "tfstateazdevops"
+    key                  = "flolie4123.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+
+#Create Get resource group
+data "azurerm_resource_group" "wsdevops" {
+  name = "ws-devops"
+}
+
+#create infra
+
+resource "azurerm_app_service_plan" "sp1" {
+  name                = "spflolie4123"
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "website" {
+  name                = "asflolie4123"
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  app_service_plan_id = azurerm_app_service_plan.sp1.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "ExternalGit"
+  }
+}
+
+#create webapp
+

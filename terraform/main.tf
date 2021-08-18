06cdc36e671b286edddc56b6b497e30a40737ad5
Florian Lierenfeld <florian.lierenfeld@capgemini.com>
2021-08-18 23:41:25 +0200

    switched to linux

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,8 @@ resource "azurerm_app_service_plan" "sp1" {
   name                = "spflolie4123"
   location            = data.azurerm_resource_group.wsdevops.location
   resource_group_name = data.azurerm_resource_group.wsdevops.name
+  kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Standard"
@@ -41,8 +43,8 @@ resource "azurerm_app_service" "website" {
   app_service_plan_id = azurerm_app_service_plan.sp1.id
 
   site_config {
-    windows_fx_version = "NODE|10-lts"
-    scm_type           = "LocalGit"
+    linux_fx_version = "NODE|10-lts"
+    scm_type         = "LocalGit"
   }
 }
 

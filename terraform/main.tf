33b6c09fc793c58226297e570f0fcdbd11280049
Florian Lierenfeld <florian.lierenfeld@capgemini.com>
2021-09-16 16:23:10 +0200

    Added Azure Provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "ws-devops"
     storage_account_name = "cgmsgtf"

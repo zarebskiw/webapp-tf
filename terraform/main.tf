d447c2811e14016dc0bf213a649eb32c2663de99
Zarebski <wojciech.zarebski@capgemini.com>
2021-10-01 16:17:44 +0200

    First version

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,11 +10,41 @@ terraform {
     resource_group_name  = "ws-devops"
     storage_account_name = "cgmsgtf"
     container_name       = "tfstateazdevops"
-    key                  = "<your unique name>.tfstate"
+    key                  = "wojzar863.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+#Get resource group
+data "azurerm_resource_group" "wsdevops" {
+  name = "ws-devops"
+}
+
+resource "azurerm_app_service_plan" "sp1" {
+  name                = "wojzar863_sp1"
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "website" {
+  name                = var.web_app_name
+  location            = data.azurerm_resource_group.wsdevops.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  app_service_plan_id = azurerm_app_service_plan.sp1.id
+
+  site_config {
+    linux_fx_version = "NODE|10-lts"
+    scm_type         = "LocalGit"
+  }
 }
 
